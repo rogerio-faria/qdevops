99a8784f08513e2ff80e547385ff88d16a39beb5
rogerio <rogerio@desq.com.br>
2021-10-06 15:03:03 -0300

    Minor improvements and bug fixes

--- mycommands/mylist.cs
+++ mycommands/mylist.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using System.IO;
-using QDevOpsBase.Server;
 using Qlik.Engine;
 using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using QDevOpsBase.Utils;
+using QDevOpsBase.Server;
 
 public class mylist : ICommand
 {
@@ -20,22 +20,43 @@ public class mylist : ICommand
 		}
 	}
 	public string CommandId => "mylist";
+	public string Group => "Custom";
 	public List<string> Content { get; set; }
 	public string Name => "My list command";
 	public int ListOrder { get; set; }
 	public bool NeedsServer => true;
 
-	public void Execute(JObject args, CommandsConfig conf)
+	public ExecuteReturn Execute(JObject args, CommandsConfig conf)
 	{
-		IEnumerable<IAppIdentifier> apps_info = conf.QlikLocation.GetAppIdentifiers();
-		if (apps_info == null)
+		ExecuteReturn eRet = new ExecuteReturn();
+
+		if (conf.QlikHub == null)
 		{
-			Console.WriteLine("Apps List returned null!");
-			return;
+			Console.WriteLine("Inaccessible Hub! Exiting...");
+
+			eRet.Result = ExecuteReturnResult.Fail;
+			eRet.LastError = new Exception("Inaccessible Hub!");
+			return eRet;
 		}
+
+		bool localmode = conf.QlikHub.IsDesktopMode();
+		Console.WriteLine();
+		Console.Write("Last Reload          App Id                                ");
+		if (!localmode) Console.WriteLine("App Name"); else Console.WriteLine();
+		Console.WriteLine("------------------------------------------------------------------------------");
+
+		eRet.Result = ExecuteReturnResult.Success;
+		IEnumerable<IAppIdentifier> apps_info = conf.QlikLocation.GetAppIdentifiers();
 		foreach (var item in apps_info)
 		{
-			Console.WriteLine($"TEST - {item.AppId}");
+			string blanks = new string(' ', 20 - (item.LastReloadTime == null ? 0 : item.LastReloadTime.Length));
+			string name = "";
+			if (!localmode) name = item.AppName;
+
+			eRet.AddReturnedData(item.AppId, item.AppName);
+			Console.WriteLine($"{item.LastReloadTime}{blanks} {item.AppId}  {name}");
 		}
+
+		return eRet;
 	}
 }
